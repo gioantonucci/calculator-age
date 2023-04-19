c27a5979627ef85030a89ef5f42c09b9315f49dc
gioantonucci <gioantonucci@hotmail.com>
2023-04-19 17:29:11 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+let day = document.getElementById('day-input')
+let month = document.getElementById('month-input')
+let year = document.getElementById('year-input')
+let button = document.getElementById('button')
+let spanDay = document.getElementById('span-day')
+let spanMonth = document.getElementById('span-month')
+let spanYear = document.getElementById('span-year')
+
+const Calculator = (day, month, year) => {
+
+}
+
