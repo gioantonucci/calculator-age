5386b3e80ecf72bad3a3c6ed700fd97dbe0e960f
gioantonucci <gioantonucci@hotmail.com>
2023-04-19 20:28:35 -0300

    button

--- project/index.js
+++ project/index.js
@@ -8,17 +8,25 @@ const Calculator = () => {
   let month = document.getElementById("month-input").value;
   let year = document.getElementById("year-input").value;
 
-  let birthDate = new Date(year, month - 1, day);
-  let today = new Date();
+  if (day == '' || month == '' || year == '') {
+    alert("falta numero");
+  } else {
+    let birthDate = new Date(year, month - 1, day);
+    let today = new Date();
 
-  let diff = today.getTime() - birthDate.getTime();
-  let age = {};
-  age.years = Math.floor(diff / (1000 * 60 * 60 * 24 * 365.25));
-  age.months = Math.floor((diff % (1000 * 60 * 60 * 24 * 365.25)) / (1000 * 60 * 60 * 24 * 30.44));
-  age.days = Math.floor((diff % (1000 * 60 * 60 * 24 * 30.44)) / (1000 * 60 * 60 * 24));
-  spanYear.innerHTML = age.years;
-  spanMonth.innerHTML = age.months;
-  spanDay.innerHTML = age.days;
+    let diff = today.getTime() - birthDate.getTime();
+    let age = {};
+    age.years = Math.floor(diff / (1000 * 60 * 60 * 24 * 365.25));
+    age.months = Math.floor(
+      (diff % (1000 * 60 * 60 * 24 * 365.25)) / (1000 * 60 * 60 * 24 * 30.44)
+    );
+    age.days = Math.floor(
+      (diff % (1000 * 60 * 60 * 24 * 30.44)) / (1000 * 60 * 60 * 24)
+    );
+    spanYear.innerHTML = age.years;
+    spanMonth.innerHTML = age.months;
+    spanDay.innerHTML = age.days;
+  }
 };
 
 calculateButton.addEventListener("click", Calculator);
