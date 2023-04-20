59f2d2f70d92620817fd1c2052cf66e533335c43
gioantonucci <gioantonucci@hotmail.com>
2023-04-20 19:14:36 -0300

    falta errores

--- project/index.js
+++ project/index.js
@@ -1,53 +1,24 @@
-let day = parseInt(document.getElementById("day-input").value);
-let month = parseInt(document.getElementById("month-input").value);
-let year = parseInt(document.getElementById("year-input").value);
-let errorDay = document.getElementById("error-day");
-let errorMonth = document.getElementById("error-month");
-let errorYear = document.getElementById("error-year");
+//BUTTON
 let calculateButton = document.getElementById("calculate-button");
-let spanDay = document.getElementById("span-day");
-let spanMonth = document.getElementById("span-month");
-let spanYear = document.getElementById("span-year");
-
-let today = new Date();
-
-calculateButton.addEventListener("click", (e) => {
-  errorDay.innerText = "";
-  errorMonth.innerText = "";
-  errorYear.innerText = "";
-  if (day == "" || month == "" || year == "") {
-    e.preventDefault();
-    if (day == "") {
-      errorDay.innerText = "this field is required";
-    }
-    if (month == "") {
-      errorMonth.innerText = "this field is required";
-    }
-    if (year == "") {
-      errorYear.innerText = "this field is required";
-    }
-  } else if (year > today.getFullYear()) {
-    e.preventDefault();
-    errorYear.innerText = "Must be in the past";
-    year = "";
-  } else if (month > 12) {
-    e.preventDefault();
-    errorMonth.innerText = "Must be a valid Month";
-    month = "";
-  } else if (day > 31) {
-    e.preventDefault();
-    errorDay.innerText = "must be a valid Day";
-    day = "";
-  } else {
-    Calculator();
-  }
-});
 
 const Calculator = () => {
+  let today = new Date();
+  //INPUTS
   let day = parseInt(document.getElementById("day-input").value);
   let month = parseInt(document.getElementById("month-input").value);
   let year = parseInt(document.getElementById("year-input").value);
-  let today = new Date();
+  //ERROR MESSAGGES
+  let errorDay = document.getElementById("error-day");
+  let errorMonth = document.getElementById("error-month");
+  let errorYear = document.getElementById("error-year");
+  //RESPONSES
+  let spanDay = document.getElementById("span-day");
+  let spanMonth = document.getElementById("span-month");
+  let spanYear = document.getElementById("span-year");
+
+  errorDay.innerText = "";
+  errorMonth.innerText = "";
+  errorYear.innerText = "";
 
   if (month > today.getMonth() + 1) {
     spanYear.innerHTML = today.getFullYear() - year - 1;
@@ -84,3 +55,5 @@ const Calculator = () => {
     spanDay.innerHTML = -(day - today.getDate());
   }
 };
+
+calculateButton.addEventListener("click", Calculator);
